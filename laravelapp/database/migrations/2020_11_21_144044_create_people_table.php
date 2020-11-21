a26ca239eff98d69f5e318dc14e91f3c9167824d
takumimae <m.t.19970830@gmail.com>
2020-11-21 23:57:23 +0900

    [Add] laravelapp

---
+++ laravelapp/database/migrations/2020_11_21_144044_create_people_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreatePeopleTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('people', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('psid');
+            $table->string('headware');
+            $table->string('mainpic');
+            $table->string('secondpic');
+            $table->string('thirdpic');
+            $table->string('rank');
+            $table->text('message');
+            $table->string('email');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('people');
+    }
+}
