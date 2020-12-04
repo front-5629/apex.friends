f73f1d49a3411f281fbae88e944dec1604a8793f
takumimae <m.t.19970830@gmail.com>
2020-12-04 21:41:49 +0900

    [Add] poststableseeder修正

--- laravelapp/database/seeders/PostsTableSeeder.php
+++ laravelapp/database/seeders/PostsTableSeeder.php
@@ -23,6 +23,21 @@ class PostsTableSeeder extends Seeder
             'require_rank' => 'dia',
             'voice_chat' => '1',
             'message' => '固定でマスター目指しましょう！',
+            'psid' => 'yamada'
+        ];
+        $post = new Post;
+        $post->fill($param)->save();
+
+        $param = [
+            'people_id' => '5',
+            'headware' => 'ps4',
+            'mainpic' => 'mirage',
+            'secondpic' => 'crypto',
+            'thirdpic' => 'octane',
+            'require_rank' => 'plat',
+            'voice_chat' => '0',
+            'message' => 'まったり',
+            'psid' => 'tanaka'
         ];
         $post = new Post;
         $post->fill($param)->save();
