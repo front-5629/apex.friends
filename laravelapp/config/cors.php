86a297ab5c09fac569fc1944aba8820993cec02d
takumimae <m.t.19970830@gmail.com>
2021-02-09 12:20:05 +0900

    cors.php paths co

--- laravelapp/config/cors.php
+++ laravelapp/config/cors.php
@@ -15,7 +15,7 @@ return [
     |
     */
 
-    'paths' => ['api/*', 'sanctum/csrf-cookie', 'login'],
+    // 'paths' => ['api/*', 'sanctum/csrf-cookie', 'login'],
 
     'allowed_methods' => ['*'],
 
